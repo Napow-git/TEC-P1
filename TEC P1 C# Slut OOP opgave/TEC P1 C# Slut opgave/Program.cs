b9be11742858222598d1f76e09062378c0bfd3ef
Napow-Git <37689142+Napow-git@users.noreply.github.com>
2023-12-15 13:15:35 +0100

    Add files via upload

---
+++ TEC P1 C# Slut OOP opgave/TEC P1 C# Slut opgave/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TEC_P1_C__Slut_opgave
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+           
+
+            Game game = new Game();
+
+
+            game.Run();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ TEC P1 C# Slut OOP opgave/TEC P1 C# Slut opgave/StateGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEC_P1_C__Slut_opgave
+{
+    class StateGame : State
+    {
+        public StateGame(Stack<State> states) : base(states) 
+        {
+            Console.WriteLine("Hello From Game State");
+        }
+    }
+}
